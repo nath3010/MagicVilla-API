69a6f58421de918350dc174102c72768a7dd5d05
nathalia <nathalia.dias3010@gmail.com>
2023-05-03 21:12:35 -0300

    add routes

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,25 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+	[Route("api/[Controller]")]
+	[ApiController]
+	public class VillaAPIController : ControllerBase
+	{
+		[HttpGet]
+		public IEnumerable<VillaDto> GetVillas()
+		{
+			return VillaStore.villaList;
+		}
+
+		[HttpGet("{id:int}")]
+		public VillaDto GetVilla(int id)
+		{
+			return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+		}
+
+	}
+}
